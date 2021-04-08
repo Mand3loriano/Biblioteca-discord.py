08006c08d61a8c78f312f646dea50e0ba9827427
Mand3loriano <81872713+Mand3loriano@users.noreply.github.com>
2021-04-08 07:51:03 -0300

    Update and rename run.py to criando_bot/run.py

--- criando_bot/run.py
+++ criando_bot/run.py
@@ -1,14 +1,14 @@
-import discord #vamos começar fazendo as importações do bot
+import discord     #vamos começar fazendo as importações do bot
 from discord.ext import commands
 
 
 Prefix = 'prefix do bot' #a variavel client define o prefix do bot
 client = commands.Bot(command_prefix=Prefix) 
 
-
+#este event escrevera, "tudo pronto" no terminal se der tudo certo
 @client.event 
 async def on_ready():
    print('tudo pronto')
 
-
+#define o token deu seu bot
 client.run('Token')
