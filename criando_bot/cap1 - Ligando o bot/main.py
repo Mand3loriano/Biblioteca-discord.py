5ae565229ca784b50bc20c1d7f4a79b960ed0199
Mand3loriano <81872713+Mand3loriano@users.noreply.github.com>
2021-04-19 20:50:56 -0300

    Create main.py

---
+++ criando_bot/cap1 - Ligando o bot/main.py
@@ -0,0 +1,15 @@
+import discord     #vamos começar fazendo as importações do bot
+from token import Token #importtando o arquivo "token.py"
+from discord.ext import commands
+
+
+Prefix = 'prefix do bot' #a variavel client define o prefix do bot
+client = commands.Bot(command_prefix=Prefix) 
+
+#este event escrevera, "tudo pronto" no terminal se der tudo certo
+@client.event 
+async def on_ready():
+   print('tudo pronto')
+
+#para esconder seu token criei um arquivo "token.py"
+client.run('Token')
