4edfeaa7ad64cebea7fbeb6be058dd5ccd875f1f
Mand3loriano <81872713+Mand3loriano@users.noreply.github.com>
2021-04-20 07:38:35 -0300

    Update main.py

--- criando bot/Cap 2 - primeiro comando/main.py
+++ criando bot/Cap 2 - primeiro comando/main.py
@@ -1,1 +1,20 @@
-print('ola mundo')
+import discord 
+from discord.ext import commands
+
+PREFIX = 'prefixo_do_seu_bot'
+client = commands.Bot(command_prefix=PREFIX)
+
+@client.event 
+async def on_ready():
+    print('Pronto para uso!\n') 
+    print(f'Nome: {client.user}\n')
+
+@client.command() # definimos o comando
+async def ola(ctx):  #aqui definimos o comando quando digitarmos /ola o bot vai responder 
+    msg = 'olá mundo'  #criamos uma váriavel para a msg
+    await ctx.send(msg) 
+            #colocamos para o bot mandar essa msg no chat do ctx ou membro 
+    
+    
+Token = 'Token_do_seu_bot'
+client.run(Token)
