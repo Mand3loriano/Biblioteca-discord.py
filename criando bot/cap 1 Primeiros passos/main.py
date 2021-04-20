60064235accc0c67cd810b0c6728fddcfa332568
Mand3loriano <81872713+Mand3loriano@users.noreply.github.com>
2021-04-19 22:34:53 -0300

    Update main.py

--- criando bot/cap 1 Primeiros passos/main.py
+++ criando bot/cap 1 Primeiros passos/main.py
@@ -1,1 +1,13 @@
-ola 
+import discord #importações do seu bot
+from discord.ext import commands
+
+PREFIX = 'prefix' #prefixo do seu bot
+client = commands.Bot(command_prefix=PREFIX)
+
+@client.event #definimos um evento
+async def on_ready():
+    print('Pronto para uso!\n') #prints no log
+    print(f'Nome: {client.user}\n')
+
+Token = 'Token'#Token do seu bot
+client.run(Token)
