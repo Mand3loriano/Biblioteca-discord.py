5e06a94072ccacb5c7b80f30849da1e0b312e9ee
Mandaloriano <augustomandaloriano@gmail.com>
2021-06-18 10:10:11 -0300

    Update main.py

--- criando bot/cap 5 - Armazenando dados/main.py
+++ criando bot/cap 5 - Armazenando dados/main.py
@@ -1,1 +1,17 @@
+import discord  # importações
+import json
 
+from discord.ext import commands
+
+with open('config.json') as e: 
+  set_config = json.load(e) # Abrindo arquivo .json
+  TOKEN = set_config['token'] # pregando o token e o prefix
+  PREFIX = set_confog['prefix']
+
+client = commands.Bot(command_prefix=PREFIX)
+
+@client.event
+async def on_ready():
+  print('hello world')
+  
+client.run(TOKEN)
