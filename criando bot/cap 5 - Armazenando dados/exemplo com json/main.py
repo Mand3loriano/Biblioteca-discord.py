990cfb3cc03f83d54b6f54e8a2f3e104c2b265c7
Mandaloriano <augustomandaloriano@gmail.com>
2021-08-09 09:18:01 -0300

    Update main.py

--- criando bot/cap 5 - Armazenando dados/exemplo com json/main.py
+++ criando bot/cap 5 - Armazenando dados/exemplo com json/main.py
@@ -1,11 +1,11 @@
-import discord  # importações
-import json
+import discord 
+import json # importando json
 
 from discord.ext import commands
 
 with open('config.json') as e: 
   set_config = json.load(e) # Abrindo arquivo .json
-  TOKEN = set_config['token'] # pregando o token e o prefix
+  TOKEN = set_config['token'] # pegando o token e o prefix
   PREFIX = set_confog['prefix']
 
 client = commands.Bot(command_prefix=PREFIX)
